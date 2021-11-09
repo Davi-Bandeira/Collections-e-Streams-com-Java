6080a4691430819ffc229c3e732f230667a6cbb5
Davi-Bandeira <davibandeira2011@hotmail.com>
2021-11-09 11:04:14 -0300

    TrabalhandoComList - Conhecendo os métodos List - Parte 2

--- src/main/java/one/digitalinnovation/list/TrabalhandoComList.java
+++ src/main/java/one/digitalinnovation/list/TrabalhandoComList.java
@@ -1,6 +1,8 @@
 package one.digitalinnovation.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class TrabalhandoComList {
@@ -40,6 +42,53 @@ public class TrabalhandoComList {
         for (Double nota : notas) System.out.println(nota);
         System.out.println();
 
+        System.out.println("Ex-07: Exiba a terceira nota adicionada: " + notas.get(2));
+        System.out.println(notas.toString());
+        System.out.println();
 
+        System.out.println("Ex-08: Exiba a menor notas: " + Collections.min(notas));
+        System.out.println();
+
+        System.out.println("Ex-09: Exiba a maior notas: " + Collections.max(notas));
+        System.out.println();
+
+        System.out.println("Ex-10: Exiba a soma dos valores: ");
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println(soma);
+        System.out.println();
+
+        System.out.println("Ex-11: Exiba a media das notas: " + (soma/notas.size()));
+        System.out.println();
+
+        System.out.println("Ex-12: Remova a nota 0: ");
+        notas.remove(0.0);
+        System.out.println(notas);
+        System.out.println();
+
+        System.out.println("Ex-13: Remova a nota da posição 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+        System.out.println();
+
+        System.out.println("Ex-14: Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+        System.out.println();
+
+        System.out.println("Ex-15: Apague toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+        System.out.println();
+
+        System.out.println("Ex-16: Confira se a lista está vazia: " + notas.isEmpty());
     }
 }
